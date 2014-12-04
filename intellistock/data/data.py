4603b34a67af289fa30b4acf652e1b417ea2b706
Csaba Kozák <kozakcsabi@gmail.com>
2014-12-04 03:06:54 +0100

    Add: historical data loading

--- intellistock/data/data.py
+++ intellistock/data/data.py
@@ -5,21 +5,20 @@ Created on Thu Nov 27 14:23:08 2014
 @author: wondercsabo
 """
 
-class DataManager:
-    """Entry point for access all data related methods. Singleton."""
-    
-    def getTrades(begin, end):
-        """Returns the trades between dates begin and end."""
-        
-    def getStocks():
-        """Returns the stocks currently stored in the database."""
+"""Entry point for access all data related methods."""
 
-    def startPolling():
-        """Starts the polling of the BSE page for new data."""
-        
-    def stopPolling():
-        """Stops polling of the BSE page."""
-        
-    def updateStocks():
-        """This will call the mediator to update the gui with new data."""
 
+def getTrades(begin, end):
+    """Returns the trades between dates begin and end."""
+        
+def getStocks():
+    """Returns the stocks currently stored in the database."""
+
+def startPolling():
+    """Starts the polling of the BSE page for new data."""
+        
+def stopPolling():
+    """Stops polling of the BSE page."""
+        
+def updateStocks():
+    """This will call the mediator to update the gui with new data."""
