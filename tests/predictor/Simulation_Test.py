bbf8e6e4754ddca1bd06d9111eecb8af1fbd0d4a
Hunyady Márton <hunyadym@gmail.com>
2015-01-13 00:18:01 +0100

    Additional tests for simulation

--- tests/predictor/Simulation_Test.py
+++ tests/predictor/Simulation_Test.py
@@ -17,3 +17,19 @@ class SimulationTest(unittest.TestCase):
     def testSell(self):
         self.simulation.sell_stock("OTP", 100)
         self.assertEquals(-100, self.simulation.stocks["OTP"])
+
+    def testMultipleBuyFromSameStock(self):
+        self.simulation.buy_stock("OTP", 100)
+        self.simulation.buy_stock("OTP", 50)
+        self.assertEquals(150, self.simulation.stocks["OTP"])
+
+    def testMultipleBuyAndSellFromSameStock(self):
+        self.simulation.buy_stock("OTP", 100)
+        self.simulation.sell_stock("OTP", 50)
+        self.assertEquals(50, self.simulation.stocks["OTP"])
+
+    def testMultipleBuyFromDifferentStocks(self):
+        self.simulation.buy_stock("MOL", 30)
+        self.simulation.buy_stock("OTP", 40)
+        self.assertEquals(30, self.simulation.stocks["MOL"])
+        self.assertEquals(40, self.simulation.stocks["OTP"])
